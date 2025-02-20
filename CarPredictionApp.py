67d614083e19d73b81bf8dfafde3c00d7a80e772
humanverified <adwaithkalathhuru@gmail.com>
2025-02-20 12:33:18 +0000

    Update CarPredictionApp.py

--- CarPredictionApp.py
+++ CarPredictionApp.py
@@ -6,28 +6,21 @@ from sklearn.preprocessing import LabelEncoder
 import gdown as gd
 import os
 
-# Use st.cache_resource to cache the model loading
 @st.cache_resource
 def load_model():
     return load('model.joblib')
 
 model = load_model()
 
-# Data Preprocessing Code
 
-# Define the path where the file will be downloaded
 file_path = 'all_car_adverts.csv'
 
-# Check if the file already exists, if not, download it
 if not os.path.exists(file_path):
-    # Download dataset (only the first time)
     url = "https://drive.google.com/uc?id=1VWcklFoYkOr2ZYqnZREBWum-2KtyM75Y&export=download"
     gd.download(url, file_path, quiet=False)
 
-# Now load the CSV file
 newfile = pd.read_csv(file_path)
 
-# Continue with the preprocessing steps as in your original code
 drop_cols = ['Unnamed: 0', 'make', 'variant', 'car_badges', 'car_sub_title', 'car_attention_grabber',
              'car_specs', 'car_seller_rating', 'reg', 'body_type', 'engine_size_unit', 'ulez',
              'car_seller_location', 'full_service', 'part_service', 'part_warranty', 'full_dealership',
@@ -45,7 +38,6 @@ for col in ['miles', 'engine_vol', 'engine_size', 'num_owner']:
 newfile['year'] = pd.to_numeric(newfile['year'], errors='coerce')
 newfile['year'] = newfile['year'].fillna(newfile['year'].mean())
 
-# Encode categorical variables
 label_encoders = {}
 original_labels = {}
 high_cardinality_cols = ['car_title', 'model', 'car_seller']
@@ -57,24 +49,18 @@ for col in high_cardinality_cols:
 
 low_cardinality_cols = ['transmission', 'fuel_type']
 newfile = pd.get_dummies(newfile, columns=low_cardinality_cols, drop_first=True)
-
-# Streamlit App
 st.title('Car Price Prediction')
 
-# Car Title Selection
 car_titles = list(original_labels['car_title'].values())
 car_title_selected = st.selectbox("Select Car", car_titles)
 
-# Dynamically filter models based on selected car title
 selected_car_index = list(original_labels['car_title'].values()).index(car_title_selected)
 selected_car_label = list(original_labels['car_title'].keys())[selected_car_index]
 filtered_models = newfile[newfile['car_title'] == selected_car_label]['model'].unique()
 
-# Model Selection
 model_options = [original_labels['model'][m] for m in filtered_models]
 model_selected = st.selectbox("Model", model_options)
 
-# Other Inputs
 car_seller_selected = st.selectbox("Car Seller", list(original_labels['car_seller'].values()))
 transmission = st.selectbox('Transmission', ['Automatic', 'Manual'])
 fuel_type = st.selectbox('Fuel Type', ['Petrol', 'Diesel', 'Electric'])
